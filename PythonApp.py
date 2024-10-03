5ec65a5505152f580901840d22493e9fa362d42f
Dominik <dominik.sawallisch@hotmail.de>
2024-10-03 10:15:39 +0200

    Migrate to Langchain

--- PythonApp.py
+++ PythonApp.py
@@ -2,24 +2,29 @@ from llm_instruction import llm_query
 import signal_credentials
 import os
 from langgraph.checkpoint.memory import MemorySaver
+import llm_langchain_connector as LLM
 
 
 
 # 1. CREATION OF APP AGENT
 # 1a We create a place where we can store the question answer pairs of a given session.
+llm = LLM.SAPLLM()
 llm_memory = MemorySaver()
-Qu_Re_Pair = llm_query("Simply Respond with Hello World")
-print(Qu_Re_Pair['answer'])
-# 
+
+# We set up LangSmith to inspect how our Application is working
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = "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"
+
+
 
 
 # With this function, we initiate a session with our Signal API. We do not define the revision_id yet. 
 signal_credentials = signal_credentials.signal_authenticate()
 signal_cookies = signal_credentials['cookies']
-signal_headers = signal_credentials['headers']https://outlook.live.com/calendar/0/view/month
+signal_headers = signal_credentials['headers']
 
 
 # 1. With this function, we call the LLM API and print the response.
-Qu_Re_Pair = llm_query("Simply Respond with Hello World")
-print(Qu_Re_Pair['answer'])
+Response = llm.invoke("Tell me a funny insult to a German")
+print(Response)
 # COntinue here
---
+++ llm_langchain_connector.py
@@ -0,0 +1,57 @@
+from langchain.llms.base import LLM
+import requests
+import llm_credentials
+from typing import List, Optional
+import llm_credentials as cred
+from langchain_core.language_models.llms import LLM
+
+
+class SAPLLM(LLM):
+    # Here, we explain the the LLM we are using is Llama 3 deployed via the SAP API.
+    @property
+    def _llm_type(self) -> str:
+        return "Llama3_LLM_via_SAP"    
+    # Get all the identifying parameters for our LLM
+    @property
+    def _identifying_params(self) -> dict:
+        auth_token, resource_group = cred.get_llm_auth().values()
+        endpoint = cred.get_llm_endpoint().values()
+        
+        return {
+            "model_name": "meta--llama3-70b-instruct",
+            'auth_token': auth_token[:5],
+            'resource_group': resource_group[5:],
+            'endpoint': endpoint
+            }
+        
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        # Reuse the logic from your function to make the request to the LLM endpoint
+        if stop is not None:
+            raise NotImplementedError("Stop tokens are not supported by this LLM API.")
+        llm_deployment_url = 'https://api.ai.prod.eu-central-1.aws.ml.hana.ondemand.com/v2/inference/deployments/d59971ba56763962'
+        endpoint = f"{llm_deployment_url}/chat/completions"
+        auth_token, resource_group = llm_credentials.get_llm_auth().values()
+        
+        input_data = {
+            "model": "meta--llama3-70b-instruct",
+            "messages": [
+                {"content": prompt, "role": "user"}
+            ]
+        }
+
+        headers = {
+            "Authorization": f"Bearer {auth_token}",
+            'ai-resource-group': resource_group,
+            "Content-Type": "application/json"
+        }
+        
+        response = requests.post(endpoint, headers=headers, json=input_data)
+        response_json = response.json()
+        # Extract the answer from the response JSON
+        if "choices" in response_json and len(response_json["choices"]) > 0:
+            response = response_json["choices"][0]["message"]["content"]
+        else:
+           raise ValueError("No answer found in the response.")
+        
+        return response
