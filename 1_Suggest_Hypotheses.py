2f25c59c82e369cb3d7f79fb5796f10362824ead
Dominik <dominik.sawallisch@hotmail.de>
2024-10-11 17:09:53 +0200

    Split into new file: 1_Suggest_Hypotheses

--- 1_Suggest_Hypotheses.py
+++ 1_Suggest_Hypotheses.py
@@ -1,14 +1,27 @@
+from llm_instruction import llm_query
+import signal_credentials as signal
+import llm_functions_main as func
+import os
+from langgraph.checkpoint.memory import MemorySaver
+import llm_langchain_connector as LLM
+from langchain_core.output_parsers import StrOutputParser
+from llm_instruction import llm_query
+# 0: SETTING UP THE ENVIRONMENT
+# 0.1 Initialize Signavio:
+#   We authenticate with Signavio and assign the cookies, and headers.
+signal_auth_data = signal.signal_authenticate()
+signal_cookies = signal_auth_data['cookies']
+signal_headers = signal_auth_data['headers']
+
+
+
 # 1: SELECT INITIAL HYPOTHESIS
 ##ideas =  f.generate_ideas(signal_cookies, signal_headers, llm)
 ##print(ideas)
 # 1.1: Generate DECLARE CONSTRAINTS
 declare_constraints = func.generate_constraints(signal_cookies, signal_headers)
 declare_constraints_str = "\n".join(declare_constraints)
-declare_constraints_str = declare_constraints_str.replace("[", " ")
-declare_constraints_str = declare_constraints_str.replace("]", " ")
-declare_constraints_str = declare_constraints_str.replace("(", " ")
-declare_constraints_str = declare_constraints_str.replace(")", " ")
-declare_constraints_str = declare_constraints_str.replace("\n", " ")
+declare_constraints_str = declare_constraints_str.replace("[", " ").replace("]", " ").replace("(", " ").replace(")", " ").replace("\n", " ")
 file_path = "./process_declare_constraints.txt"
 with open(file_path, "w") as file:
     file.write(declare_constraints_str)
@@ -33,4 +46,10 @@ sys_message_user = sysgen2
 
 answer = llm_query(sys_message_gen, sys_message_user)
 answer = answer['answer']
-print(answer)
+
+hypothesis = answer.split("**Hypothesis 1:**")[1].split("**Hypothesis 2:**")[0]
+# hypothesis now contains only the content between Hypothesis 1 and Hypothesis 2
+print(hypothesis)
+
+# For now, We just select the first hypothesis that it provides
+
