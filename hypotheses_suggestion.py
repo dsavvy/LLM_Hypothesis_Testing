540b966f1afa8fe4e093d2e06800a9a53ce3c89a
Dominik <dominik.sawallisch@hotmail.de>
2025-01-15 11:41:41 +0100

    sync

--- hypotheses_suggestion.py
+++ hypotheses_suggestion.py
@@ -47,21 +47,21 @@ def selectDirection():
 
     # Use `st.button` with a callback to set session state directly
     if st.button("Check Data Quality", key="data_check"):
-        st.session_state["direction"] = "data_check"
-        app.response("We will Check Data Quality")
+        direction = "Check Data Quality"
+        st.session_state.messages.append({"role": "user", "content":"I want to check Data Quality"})
     if st.button("Check Process Conformance", key="conf_check"):
         st.session_state["direction"] = "conf_check"
-        app.response("We will Check Process Conformance")
+        direction = "Check Process Conformance"
+        st.session_state.messages.append({"role": "user", "content":"I want to Check Process Conformance"})
     if st.button("Enhance Process Model", key="enhance_model"):
-        st.session_state["direction"] = "enhance_model"
-        app.response("We will look for optimization opportunities")
-    
+        direction = "Enhance Process Model"
+        st.session_state.messages.append({"role": "user", "content":"I want to look for optimization opportunities"})
     # Check and provide fallback if no valid direction is selected
     if not st.session_state["direction"]:
         app.response("Please select hypothesis 1, 2, or 3 by the respective button.")
-        st.session_state["direction"] = "Error: No direction selected."
+        direction = "Error: No direction selected."
     
-    return st.session_state["direction"]
+    return direction
 
 
 
